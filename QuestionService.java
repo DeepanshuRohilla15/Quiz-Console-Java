fe911433864f19ba67fec2dfa8f745159bffefaa
Deepanshu Rohilla <20001001027deepanshu@dcrustm.org>
2025-10-18 23:35:52 +0530

    Play Quiz

--- QuestionService.java
+++ QuestionService.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
+
 public class QuestionService {
     
     Question[] questions = new Question[3];
+    String selection[] = new String[5];
 
     public QuestionService()
     {
@@ -9,11 +12,27 @@ public class QuestionService {
         questions[2] = new Question(3, "Who is fav ?", "iron Man", "Captain", "hulk", "Black", "iron Man");
     }
 
-    public void displayQuestions()
+    public void playQuiz()
     {
+        int i = 0;
         for(Question q : questions)
         {
+             System.out.println("Question No. : " + q.getId());
              System.out.println(q.getQuestion());
+             System.out.println(q.getOpt1());
+             System.out.println(q.getOpt1());
+             System.out.println(q.getOpt2());
+             System.out.println(q.getOpt3());
+             System.out.println(q.getOpt4());
+            Scanner sc = new Scanner(System.in);
+
+            selection[i] = sc.nextLine();
+            i++;
+        }
+
+        for(String s : selection)
+        {
+            System.out.println(s);
         }
            
     }
