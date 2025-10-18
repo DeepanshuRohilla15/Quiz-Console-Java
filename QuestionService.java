6a9ca92f7e0712c3791c608d50e1a54c8b128d93
Deepanshu Rohilla <20001001027deepanshu@dcrustm.org>
2025-10-18 22:04:48 +0530

    Hardcoded them and display them

--- QuestionService.java
+++ QuestionService.java
@@ -1,4 +1,20 @@
 public class QuestionService {
     
-    Question[] questions = new Question[5];
+    Question[] questions = new Question[3];
+
+    public QuestionService()
+    {
+        questions[0] = new Question(1, "What is best ?", "Java", "Cpp", "Python", "Csharp", "Java");
+        questions[1] = new Question(2, "Who is best ?", "Tony", "Steve", "Hulk", "Pepper", "Tony");
+        questions[2] = new Question(3, "Who is fav ?", "iron Man", "Captain", "hulk", "Black", "iron Man");
+    }
+
+    public void displayQuestions()
+    {
+        for(Question q : questions)
+        {
+             System.out.println(q.getQuestion());
+        }
+           
+    }
 }
